4bc6edb3bb2aba1d070ce7a94948cc3b9ebdf2c0
Martin Uhrin <martin.uhrin@gmail.com>
2020-01-25 21:46:25 +0100

    Added '_running' field to Process

--- mincepy/process.py
+++ mincepy/process.py
@@ -1,7 +1,6 @@
 import contextlib
 import uuid
 
-from . import depositors
 from . import types
 
 __all__ = ('Process',)
@@ -10,7 +9,7 @@ __all__ = ('Process',)
 class Process(types.Archivable):
     TYPE_ID = uuid.UUID('bcf03171-a1f1-49c7-b890-b7f9d9f9e5a2')
     STACK = []
-    ATTRS = ('_name',)
+    ATTRS = ('_name', '_running')
 
     @classmethod
     def current_process(cls):
@@ -18,11 +17,10 @@ class Process(types.Archivable):
             return None
         return cls.STACK[-1]
 
-    DEFINING_ATTRIBUTES = ('_name',)
-
     def __init__(self, name: str):
         super(Process, self).__init__()
         self._name = name
+        self._running = False
 
     def __eq__(self, other):
         if not isinstance(other, Process):
@@ -37,8 +35,12 @@ class Process(types.Archivable):
     @contextlib.contextmanager
     def running(self):
         self.STACK.append(self)
-        yield
-        if self.STACK[-1] != self:
-            raise RuntimeError("Someone has corrupted the process stack!\n"
-                               "Expected to find '{}' on top but found:{}".format(self, self.STACK))
-        self.STACK.pop()
+        self._running = True
+        try:
+            yield
+        finally:
+            if self.STACK[-1] != self:
+                raise RuntimeError("Someone has corrupted the process stack!\n"
+                                   "Expected to find '{}' on top but found:{}".format(self, self.STACK))
+            self._running = False
+            self.STACK.pop()
