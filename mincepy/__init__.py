58af2d3bac315e0a26ba79f1314ed45aecbfaf82
Martin Uhrin <martin.uhrin@gmail.com>
2020-03-11 17:18:20 +0100

    Removed testing from mincepy module
    This way users that don't install pytest (and other dev necessary
    dependencies) can still use it.

--- mincepy/__init__.py
+++ mincepy/__init__.py
@@ -17,10 +17,9 @@ from .version import *
 from . import analysis
 from . import common_helpers
 from . import mongo
-from . import testing
 from . import utils
 
-_ADDITIONAL = ('analysis', 'mongo', 'buitins', 'common_helpers', 'testing', 'utils')
+_ADDITIONAL = ('analysis', 'mongo', 'buitins', 'common_helpers', 'utils')
 
 __all__ = (archives.__all__ + comparators.__all__ + depositors.__all__ + exceptions.__all__ +
            historians.__all__ + convenience.__all__ + process.__all__ + types.__all__ +
