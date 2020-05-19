791f2833bc749425da8a478a4b8102441d199755
Martin Uhrin <martin.uhrin@gmail.com>
2020-05-19 11:11:36 +0200

    Added analytics ID

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -105,6 +105,7 @@ html_theme = 'alabaster'
 #
 # html_theme_options = {}
 html_theme_options = {
+    'analytics_id': 'UA-17296547-2',
     'codecov_button': True,
     'description': 'Python object storage with versioning made simple',
     'github_button': True,
