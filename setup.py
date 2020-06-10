4baaf1b96eb1eb01def31be433f6401d08ac65b8
Martin Uhrin <martin.uhrin@gmail.com>
2020-06-10 16:44:47 +0200

    Added support for py38 in setup.py

--- setup.py
+++ setup.py
@@ -23,6 +23,7 @@ setup(
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
     ],
     keywords='database schemaless nosql orm object-store concurrent optimistic-locking',
     install_requires=[
