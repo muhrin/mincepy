493d35df09198fa5e52ba69203586d6dcc48dcb2
Martin Uhrin <martin.uhrin@gmail.com>
2020-08-14 17:46:16 +0200

    Added test types to automatically provided types
    This way databases populated with test data can automatically be
    understood without having to register the test classes.

--- mincepy/provides.py
+++ mincepy/provides.py
@@ -1,6 +1,7 @@
 from . import common_helpers
 from . import builtins
 from . import refs
+from . import testing
 
 
 def get_types():
@@ -9,5 +10,6 @@ def get_types():
     types.extend(builtins.HISTORIAN_TYPES)
     types.extend(common_helpers.HISTORIAN_TYPES)
     types.extend(refs.HISTORIAN_TYPES)
+    types.extend(testing.HISTORIAN_TYPES)
 
     return types
