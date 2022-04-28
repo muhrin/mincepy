1e25312732c2a85eeb6600bd10399471d5802b2a
Martin Uhrin <martin.uhrin@gmail.com>
2022-04-28 12:41:41 +0200

    Release 0.15.27

--- mincepy/version.py
+++ mincepy/version.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 author_info = (('Martin Uhrin', 'martin.uhrin.10@ucl.ac.uk'),)
-version_info = (0, 15, 26)
+version_info = (0, 15, 27)
 
 __author__ = ', '.join('{} <{}>'.format(*info) for info in author_info)
 __version__ = '.'.join(map(str, version_info))
